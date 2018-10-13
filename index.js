e2157e48840d9ffdb2815c782b3f0466a69b17d9
Schalk Venter <venterschalk@gmail.com>
2018-10-13 16:20:13 +0200

    Fixes issue with request require expression

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { request } = require('request')
+const request = require('request')
 const { IncomingWebhook } = require('@slack/client');
 
 const MEETUP_URL = 'https://api.meetup.com/Codebridge/events';
